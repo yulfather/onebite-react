2ccc23a50fb0563bc34d0e31e4fe0e71c7171fd6
yulfather <soulplus82@gmail.com>
2026-01-22 17:20:17 +0900

    section05완료

--- section05/src/App4.jsx
+++ section05/src/App4.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
 import Register from "./components/Register";
 import { InputExample1, InputExample2 } from "./components/InputExample";
-import LoginForm from "./components/LoginForm";
 import NewRegister from "./components/NewRegister";
 import AppRef from "./components/AppRef";
 import RefRegister from "./components/RefRegister";
+import HookExam from "./components/HookExam";
 
 // State로 사용자 입력 관리하기1
 
 function App4() {
   return (
     <>
+      <HookExam />
       <RefRegister />
       <AppRef />
       <NewRegister />
-      <LoginForm />
       <InputExample2 />
       <InputExample1 />
       <Register />
---
+++ section05/src/hooks/useInput.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function useInput() {
+  const [input, setInput] = useState("");
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return [input, onChange];
+}
