4cc38c3592546abe9f564059016e72f5f631649a
yulfather <soulplus82@gmail.com>
2026-01-27 18:13:30 +0900

    todo list review

--- test03/src/components/List.jsx
+++ test03/src/components/List.jsx
@@ -1,13 +1,35 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 function List({ todos }) {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="item_wrapper">
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoItem key={todo.id} {...todo} />;
         })}
       </div>
